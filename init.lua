be5e5fd7ffcd22253fc92aa021e0284f1ca3ab39
javierrollan <jrollanb@gmail.com>
2023-12-26 02:02:36 +0100

    lualine added and nvims-configs file created

--- init.lua
+++ init.lua
@@ -1,18 +1,3 @@
-vim.cmd("set encoding=utf-8")
-vim.cmd("set nocompatible")
-vim.cmd("set showmatch")
-vim.cmd("set ignorecase")
-vim.cmd("set mouse=v")
-vim.cmd("set hlsearch")
-vim.cmd("set incsearch")
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set autoindent")
-vim.cmd("set number")
-vim.g.mapleader = " "
-
 -- lazy.nvim plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -27,4 +12,5 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("nvim-configs")
 require("lazy").setup("plugins")

491b5157f53caaa1c75f808ac8a5c79c149b902c
javier <jrollanb@gmail.com>
2024-11-17 02:19:18 +0100

    Automated commit: 2024-11-17 02:19:18

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,3 +1,18 @@
 return {
-    {'lewis6991/gitsigns.nvim'}
+    {
+        'lewis6991/gitsigns.nvim',
+        config = function ()
+            local gitsigns = require('gitsigns')
+            gitsigns.setup {
+                  signs = {
+                    add          = { text = '┃' },
+                    change       = { text = '┃' },
+                    delete       = { text = '_' },
+                    topdelete    = { text = '‾' },
+                    changedelete = { text = '~' },
+                    untracked    = { text = '┆' },
+                }
+            }
+        end,
+    }
 }

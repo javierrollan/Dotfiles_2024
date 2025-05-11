32ddefad0cf7184a542a63783d5ebe2be800014d
javier <jrollanb@gmail.com>
2025-05-11 12:01:02 +0200

    Automated commit: 2025-05-11 12:01:02

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,4 +2,5 @@
 vim.keymap.set('n', '<C-n>', ':tabnext<CR>') -- Next Tab
 vim.keymap.set('n', '<C-m>', ':tabprevious<CR>') -- Previous Tab
 vim.keymap.set('n', '<leader>t', ':Neotree toggle<CR>')
+vim.keymap.set('n', '<leader>f', ':Neotree focus<CR>')
 vim.keymap.set('n', 'p', 'pgvy', { noremap = true, silent = true}) -- "p" now is mapped to "pgvy" so it yanks latest pasted line 

98e40f2ab951fe9c2d16834c02d66d16fe33628c
javier <jrollanb@gmail.com>
2025-06-01 20:32:39 +0200

    Automated commit: 2025-06-01 20:32:39

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -13,8 +13,8 @@ keymap.set('n', '>', '>gv')
 -- Split Windows -----------------------------------------------------------------------------------------------------------------
 keymap.set('n', '<leader>sv', ':vsplit<CR>', opts) -- Split Vertical
 keymap.set('n', '<leader>sh', ':split<CR>', opts) -- Split Horizontal
-keymap.set('n', '<leader>v+', '<C-w>|', opts) -- Zoom Split Vertical
-keymap.set('n', '<leader>h+', '<C-w>_', opts) -- Zoom Split Horizontal
+keymap.set('n', '<leader>+v', '<C-w>|', opts) -- Zoom Split Vertical
+keymap.set('n', '<leader>+h', '<C-w>_', opts) -- Zoom Split Horizontal
 keymap.set('n', '<leader>-', '<C-w>=', opts) -- Exit Zoom Split 
 
 -- Remap Move -------------------------------------------------------------------------------------------------------------------
@@ -22,6 +22,10 @@ keymap.set('n', '<C-j>', ':move +1<CR>') -- move line down
 keymap.set('n', '<C-k>', ':move -2<CR>') -- move line up
 keymap.set('n', '<leader>h', '<C-w>h', opts) -- Change Focus to left pane
 keymap.set('n', '<leader>l', '<C-w>l', opts) -- change Focus to right pane
+keymap.set('n', '<leader>j', '<C-w>j', opts) -- Change Focus to bottom pane
+keymap.set('n', '<leader>k', '<C-w>k', opts) -- change Focus to upper pane
+
+-- Save & Quit ------------------------------------------------------------------------------------------------------------------
 keymap.set('n', '<leader>x', ':w<CR>', opts) -- Save file
 keymap.set('n', '<leader>q', ':q<CR>', opts) -- Quit
 keymap.set('n', '<leader>wq', ':wqa<CR>', opts) -- Save all and quit

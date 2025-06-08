2f0b5c42f404b06eee792eaad3a3fd3063b25f8b
javier <jrollanb@gmail.com>
2025-06-08 11:14:18 +0200

    Automated commit: 2025-06-08 11:14:18

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -10,7 +10,7 @@ return {
             pickers = {
                 find_files = {
                     hidden = true,
-                    find_command = {"rg", "--files","--hidden","--glob","!**/.git/**"},
+                    find_command = {"rg", "--files",  "--no-ignore-vcs", "--hidden","--glob","!**/.git/**"},
                 }
             }
         })

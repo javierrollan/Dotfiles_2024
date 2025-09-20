8cbef56e4acb443e294c2266ee127723de14dcde
javier <jrollanb@gmail.com>
2025-09-20 16:10:12 +0200

    Automated commit: 2025-09-20 16:10:12

--- nvim/lua/config/filetype.lua
+++ nvim/lua/config/filetype.lua
@@ -4,6 +4,7 @@ vim.filetype.add({
         [".*/group_vars/.*.yaml"]   = "yaml.ansible",
         [".*/handlers/.*.yaml"]     = "yaml.ansible",
         [".*/ansible/.*.yaml"]      = "yaml.ansible",
-        [".*.yaml.j2"]              = "yaml"
+        [".*.yaml.j2"]              = "yaml",
+        [".*.trace"]                = "strace"
     }
 })
